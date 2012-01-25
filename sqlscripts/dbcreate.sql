c80896ccffad4495e40cab7d8f3b233289ddc2d2
tm1 <testmode1@gmail.com>
2012-01-25 13:31:28 +0600

    Create SQL roles

--- sqlscripts/dbcreate.sql
+++ sqlscripts/dbcreate.sql
@@ -1,6 +1,13 @@
 /********************* ROLES **********************/
 
+CREATE ROLE ADMINISTRATOR;
+CREATE ROLE CASHIER;
+CREATE ROLE CLEANER;
+CREATE ROLE ENTRANCE;
+CREATE ROLE KIOSK;
 CREATE ROLE RDB$ADMIN;
+CREATE ROLE REPLDATA;
+CREATE ROLE REPLMETA;
 /********************* UDFS ***********************/
 
 /****************** GENERATORS ********************/
